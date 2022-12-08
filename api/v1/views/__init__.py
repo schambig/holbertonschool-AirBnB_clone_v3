362e3edabcdd2f5a3249f5226add45c76175daf3
Drixner <drixnerrafael1@gmail.com>
2022-12-08 00:21:38 -0500

    feat: create a blueprint instance `app_views`

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,7 @@
+"""create variable app_views for instance of Blueprint"""
+from flask import Blueprint
+
+
+app_views = Blueprint("app_views", __name__, url_prefix='api/v1')
+# import flask view:
+from api.v1.view.index import *
