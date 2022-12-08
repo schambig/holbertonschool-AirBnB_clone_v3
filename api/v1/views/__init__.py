b6daf07a9227f89ca77700f295bfb4d1e6e4a2c9
Drixner <drixnerrafael1@gmail.com>
2022-12-08 00:40:26 -0500

    fix: correct import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python3
 """create variable app_views for instance of Blueprint"""
 from flask import Blueprint
 
 
 app_views = Blueprint("app_views", __name__, url_prefix='api/v1')
 # import flask view:
-from api.v1.view.index import *
+from api.v1.views.index import *
