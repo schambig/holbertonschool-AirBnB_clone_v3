3c8b4eb0cc54ccb5e8531287c0f220e860eedc0f
Salomon Chambi <salomonchambi@gmail.com>
2022-12-08 16:48:19 -0500

    feat: Retrieve a State object using an id

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -17,3 +17,17 @@ def return_states():
     for key, value in states.items():
         states_list.append(value.to_dict())
     return jsonify(states_list)
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def return_states_id(state_id):
+    ''' Retrieve a State object using a specific id '''
+    # save all the objects in State class from database
+    states = storage.all(State)
+    for key, values in states.items():
+        # check if id passed is linked to State object
+        # if so, return the State object
+        if states[key].id == state_id:
+            return value.to_dict()
+    # raise error 404 if id is not liked to State object
+    abort(404)
