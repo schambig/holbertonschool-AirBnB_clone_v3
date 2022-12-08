37d3c828a4d75d29cbd9bfa1a6497c8c4ec915e1
Drixner <drixnerrafael1@gmail.com>
2022-12-08 00:32:12 -0500

    fix: correct argument in route decorator

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route("/status", method=['GET'])
+@app_views.route("/status", methods=['GET'])
 def return_status():
     """Return status of GET request"""
     return jsonify({'status': 'OK'})
