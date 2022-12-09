cdd2dbcd9f0d8f617112388df7d37aa9408788f9
Salomon Chambi <salomonchambi@gmail.com>
2022-12-09 17:48:21 -0500

    feat: retrieve a `Review` object using its id

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -27,3 +27,13 @@ def return_reviews(place_id):
     for review in place.reviews:
         reviews.append(review.to_dict())
     return jsonify(reviews)
+
+
+@app_views.route('/reviews/<review_id>', methods=['GET'], strict_slashes=False)
+def return_review_id(review_id):
+    ''' Retrieve a Review object using its id, use GET http method '''
+    # retrieve the object based on the class and its ID, or None if not found
+    review = storage.get(Review, review_id)
+    if review is None:
+        abort(404)
+    return jsonify(review.to_dict())
