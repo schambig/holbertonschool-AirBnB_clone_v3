d72b41c76cfafa9e974d14ef57ad8bb623bd9828
Salomon Chambi <salomonchambi@gmail.com>
2022-12-08 19:46:11 -0500

    feat: create a State object

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -5,7 +5,8 @@ Create a new view for State objects that handles all default HTTP methods
 from models.state import State
 from models import storage
 from api.v1.views import app_views
-from flask import jsonify, abort, make_response
+from flask import jsonify, abort, request
+# from flask import make_response
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
@@ -44,5 +45,23 @@ def delete_states_id(state_id):
     else:
         storage.delete(state)
         storage.save()
-        # return and empty dictionary with the status code 200
-        return make_response(jsonify({}), 200)
+        # return an empty dictionary with the status code 200
+        return (jsonify({}), 200)
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def post_state():
+    ''' Create a State object '''
+    # transform the HTTP body request to a python dictionary
+    body = request.get_json()
+    if body is None:
+        return (jsonify('error': 'Not a JSON'), 400)
+    # create a new instance of State and pass body dict as **kwargs
+    obj = State(**body)
+
+    if 'name' not in body:
+        return (jsonify('error': 'Mising name'), 400)
+    else:
+        storage.new(obj)
+        storage.save()
+        return (jsonify(obj.to_dict()), 201)
